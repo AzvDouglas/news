05b1d7eb12ef6ffa01aab4abdee280bd1ecf7c13
azvdouglas <douglasbep2014@gmail.com>
2024-04-11 14:56:43 -0300

    noticias.index

--- resources/views/livewire/welcome/navigation.blade.php
+++ resources/views/livewire/welcome/navigation.blade.php
@@ -1,7 +1,7 @@
 <nav class="flex justify-end flex-1 -mx-3">
     @auth
         <a
-            href="{{ url('/noticias') }}"
+            href="{{ url('/dashboard') }}"
             class="rounded-md px-3 py-2 text-white ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
         >
             Suas Notícias
