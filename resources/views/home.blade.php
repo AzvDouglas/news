afae112d3eb134f9cbbd5e255b907362281e97e7
azvdouglas <douglasbep2014@gmail.com>
2024-04-11 14:46:31 -0300

    testando toggle Alpine & Livewire

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notícias</title>
+    
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
+
 </head>
 <body class="font-sans bg-gray-100">
     <!-- Navbar -->
 	@include('components.navbar')
 
     <!-- Conteúdo da Página -->
-    <div class="container px-4 py-8 mx-auto">
+    <div class="container px-4 py-8 mx-auto" x-data="{ showNews: true }">
         <h1 class="mb-4 text-3xl font-bold">Últimas Notícias</h1>
-        <button id="toggleButton" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">Toggle Notícias</button>
+        <button @click="showNews = !showNews" class="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">Toggle Notícias</button>
 
-        <div id="noticiasContainer" class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <!-- Contêiner de notícias com diretiva x-show -->
+        <div x-show="showNews" class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
             <!-- Notícias -->
             @foreach($noticias as $noticia)
             <!-- Renderiza o componente Noticia com a instância do modelo Noticia -->
@@ -34,13 +39,8 @@
     
     <!-- Scripts -->
     @livewireScripts
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script>
-    $(document).ready(function(){
-        $('#toggleButton').click(function(){
-            $('#noticiasContainer').toggle(); // Alterna a visibilidade do contêiner de notícias
-        });
-    });
-</script>
+    <script>
+        window.Alpine || console.error('Alpine.js não foi carregado.');
+    </script>
 </body>
 </html>
